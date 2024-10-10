b8385488d802a5ca64581de5b9c92bf13d058d27
jakobriccabona <riccabona.jakob@gmail.com>
2024-10-10 15:01:27 +0200

    update

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import menten_gcn.decorators as decs
 
 from spektral.layers import *
 from tensorflow import keras
+from keras.regularizers import l2
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
 from tensorflow.keras import regularizers
@@ -21,9 +22,15 @@ from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_curve, confusion_matrix
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import roc_curve, roc_auc_score
+
+plt.rcParams.update({'font.size': 15})
+plt.rcParams['axes.linewidth'] = 2
 
 def load_npz_files():
-    data = np.load('merged_file.npz')
+    data = np.load('../data-generation/data/merged_file.npz')
     Xs = data['Xs']
     As = data['As']
     Es = data['Es']
@@ -40,7 +47,7 @@ Es = np.asarray(Es)
 outs = np.asarray(outs)
 
 # Pick decorators
-decorators = [decs.SimpleBBGeometry(use_nm = False), decs.Rosetta_Ref2015_TwoBodyEneriges(individual=True, score_types=[ScoreType.fa_rep, ScoreType.fa_atr, ScoreType.fa_sol, ScoreType.lk_ball_wtd, ScoreType.fa_elec, ScoreType.hbond_sr_bb, ScoreType.hbond_lr_bb, ScoreType.hbond_bb_sc, ScoreType.hbond_sc])]
+decorators = [decs.SequenceSeparation(ln = True), decs.SimpleBBGeometry(use_nm = False), decs.Rosetta_Ref2015_TwoBodyEneriges(individual=True, score_types=[ScoreType.fa_rep, ScoreType.fa_atr, ScoreType.fa_sol, ScoreType.lk_ball_wtd, ScoreType.fa_elec, ScoreType.hbond_sr_bb, ScoreType.hbond_lr_bb, ScoreType.hbond_bb_sc, ScoreType.hbond_sc])]
 
 data_maker = mg.DataMaker(decorators=decorators,
                            edge_distance_cutoff_A=8.0,
@@ -73,15 +80,15 @@ E_ros = E_reshaped.reshape(-1, *num_features)
 # Define GCN model
 X_in, A_in, E_in = data_maker.generate_XAE_input_layers()
 
-L1 = ECCConv(15, activation=None)([X_in, A_in, E_in])
+L1 = ECCConv(30, activation=None)([X_in, A_in, E_in])
 L1_bn = BatchNormalization()(L1)
 L1_act = Activation('relu')(L1_bn)
 L1_drop = Dropout(0.2)(L1_act)
-L2 = ECCConv(15, activation=None)([L1_drop, A_in, E_in])
+L2 = GATConv(30, attn_heads=4, concat_heads=True, activation=None)([L1_drop, A_in])
 L2_bn = BatchNormalization()(L2)
 L2_act = Activation('relu')(L2_bn)
 L2_drop = Dropout(0.2)(L2_act)
-L3 = GlobalSumPool()(L2_drop)
+L3 = GlobalMaxPool()(L2_drop)
 L4 = Flatten()(L3)
 output = Dense(1, name="out", activation="sigmoid", kernel_regularizer=l2(0.01))(L4)
 
@@ -92,4 +99,49 @@ model.summary()
 
 # Train the model
 history = model.fit(x=[X_ros, A_ros, E_ros], y=y_ros, batch_size=200, epochs=100, validation_data=([X_val, A_val, E_val], y_val))
-model.save("3D-model.keras")
+model.save("3D-model-new.keras")
+
+#further validation
+y_pred_prob = model.predict([X_val, A_val, E_val])
+y_pred = (y_pred_prob > 0.5).astype(int) 
+
+mcc = matthews_corrcoef(y_val, y_pred)
+fpr, tpr, thresholds = roc_curve(y_val, y_pred_prob)
+auc = roc_auc_score(y_val, y_pred_prob)
+precision, recall, thresholds = precision_recall_curve(y_val, y_pred_prob)
+cm = confusion_matrix(y_val, y_pred)
+cmn = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+#Plottings
+plt.plot(history.history['loss'], label='Training Loss', color='royalblue')
+plt.plot(history.history['val_loss'], label='Validation Loss', color='goldenrod')
+plt.title('Model Loss Over Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('history.png')
+plt.clf()
+
+plt.plot(recall, precision)
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.savefig('precision-recall.png')
+plt.clf()
+
+plt.plot(fpr, tpr, label='ROC curve', color='royalblue')
+plt.xscale('log')  # Set x-axis to logarithmic scale
+plt.xlabel('Log False Positive Rate (FPR)')
+plt.ylabel('True Positive Rate (TPR)')
+plt.xlim([0.00001, 1])  # Set limits for the logarithmic scale
+plt.ylim([0.0, 1.05])
+plt.title('Log ROC Curve')
+plt.savefig('log-roc.png')
+plt.clf()
+
+sns.heatmap(cmn, annot=True, fmt='g', cmap='Blues', xticklabels=['Non-Proline', 'Proline'], yticklabels=['Non-Proline', 'Proline'])
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.savefig('conf-m.png')
+plt.clf()
