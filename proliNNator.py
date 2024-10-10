561643eae66937103e628b63a62ea3c29b497f0b
jakobriccabona <riccabona.jakob@gmail.com>
2024-10-10 15:20:53 +0200

    remove the 1D network

--- proliNNator.py
+++ proliNNator.py
@@ -20,63 +20,17 @@ from pyrosetta.rosetta.core.scoring.methods import EnergyMethodOptions
 import keras
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import StandardScaler
-from spektral.layers import ECCConv, GlobalSumPool
+from spektral.layers import ECCConv, GlobalMaxPool, GATConv
 
 import menten_gcn as mg
 import menten_gcn.decorators as decs
-
-# functions
-def extract_features(pose, indices):
-    # Extract sequence
-    sequence = pose.sequence()
-    
-    # Extract secondary structure using DSSP
-    DSSP = pyrosetta.rosetta.protocols.moves.DsspMover()
-    DSSP.apply(pose)
-    dssp = pose.secstruct()
-    
-    # Extract SASA
-    sasa = pyrosetta.rosetta.core.scoring.sasa.SasaCalc()
-    sasa.calculate(pose)
-    residue_sasa_list = sasa.get_residue_sasa()
-    
-    # Extract Scoring
-    sfxn = pyrosetta.get_fa_scorefxn()
-    emo = EnergyMethodOptions()
-    emo.hbond_options().decompose_bb_hb_into_pair_energies(True)
-    sfxn.set_energy_method_options(emo)
-    sfxn(pose)
-    scores_df = pd.DataFrame(pyrosetta.bindings.energies.residue_total_energies_array(pose.energies(), residue_selection=range(1, len(sequence)+1)))
-    scores_df = scores_df.drop(columns=['fa_intra_rep','fa_intra_sol_xover4','dslf_fa13','rama_prepro','p_aa_pp','fa_dun','omega','pro_close', 'yhh_planarity', 'ref', 'total_score'])
-     
-    # Get chain information for each residue
-    residue_chains = [pose.pdb_info().chain(i+1) for i in range(pose.total_residue())]
-    
-    filtered_data = []
-    for i in indices:
-        if i >= 2 and i + 2 < len(sequence):
-            # Check if the residues are in the same chain
-            if len(set(residue_chains[i-2:i+3])) == 1:
-                seq_chunk = sequence[i-3:i+4]
-                dssp_chunk = dssp[i-3:i+4]
-                sasa_chunk = [residue_sasa_list[j] if j < len(residue_sasa_list) else None for j in range(i-2, i+5)]
-                energy_chunk = scores_df.iloc[i-3:i+4].to_dict(orient='records')
-
-                filtered_data.append((seq_chunk, dssp_chunk, sasa_chunk, energy_chunk))
-    
-    return filtered_data
-
-def OneHotEncoder(df, column):
-    seq = pd.DataFrame(df[column].apply(list).tolist())
-    encoded = pd.get_dummies(seq)
-    return encoded
 
 # main
 def main():
     # Argument parser setup
     parser = argparse.ArgumentParser(description='ProliNNator is a tool that predicts Proline probabilties based on pretrained neural networks. \n Contact: Jakob.Riccabona@medizin.uni-leipzig.de')
     parser.add_argument('-i', '--input', type=str, required=True, help='Path to the input PDB file')
-    parser.add_argument('-m', '--model', type=str, default='3D-model.keras', help='Path to the model')
+    parser.add_argument('-m', '--model', type=str, default='3D-model-new.keras', help='Path to the model')
     parser.add_argument('-o', '--output', type=str, default='output.pdb', help='Name of the output PDB file (default: output.pdb)')
     parser.add_argument('--csv', type=str, default='output.csv', help='Filename to save a csv file with the probabilities')
     parser.add_argument('--ramachandran', type=str, help='Filename to save a Ramachandran plot with probabilities as a PNG')
@@ -100,142 +54,69 @@ def main():
 
     # Load the ML model
     mod = args.model
-    if '1D' in mod:
-        model = load_model(mod)
-        # Calculate features from pdb
-        features = extract_features(pose, [i for i in range(3, len(pose.sequence())-3)])
-         # Format data to relevant features
-        data = pd.DataFrame(features, columns=['Sequence','DSSP','SASA','Scores'])
-        sec_onehot = OneHotEncoder(data, "DSSP")
-        converted_sasa = np.array([np.array(row) for row in data['SASA'].values])
-        converted_sasa = converted_sasa.astype(float)
-
-        # Parse the strings into lists of dictionaries
-        parsed_scores = [row for row in data['Scores']]
-        # Flatten each list of dictionaries into a single dictionary and then into a 1D array
-        flattened_scores = []
-        for score_list in parsed_scores:
-            flattened_score = []
-            for score_dict in score_list:
-                flattened_score.extend(score_dict.values())
-            flattened_scores.append(flattened_score)
-        # Convert the list of flattened scores into a 2D numpy array
-        data_array = np.array(flattened_scores)
-
-        # Normalize SASA and data_array
-        scaler = StandardScaler()
-        converted_sasa_n = scaler.fit_transform(converted_sasa)
-        data_array_n = scaler.fit_transform(data_array)
-
-        # Final features
-        features = np.concatenate((sec_onehot, converted_sasa_n, data_array_n), axis=-1)
-        features = np.delete(features, [9,10,11,24,48,49,50,51,52,53,54,55,56], axis=1)
-
-        # Run prediction
-        y_pred = model.predict(features)
-        
-        if args.csv:
-            rows = []
-            for c in range(1, pose.pdb_info().num_chains() + 1):
-                chain_start = pose.conformation().chain_begin(c)
-                chain_end = pose.conformation().chain_end(c)
-                for i in range(chain_start + 3, chain_end - 3):
-                    if i-1 < len(y_pred):
-                        row = {
-                            'chain': pose.pdb_info().chain(i),
-                            'amino_acid': pose.residue(i).name(),
-                            'position_number': i,
-                            'probability': y_pred[i-1]
-                        }
-                        rows.append(row)
-            df = pd.DataFrame(rows)
-            df.to_csv(args.csv, index=False)
-
-        # Map y_pred onto crystal structure and save pdb
-        # Set all bfactors to zero
-        for i in range(1, len(pose.sequence()) + 1):
-            for j in range(1, pose.residue(i).natoms() + 1):
-                pose.pdb_info().bfactor(i, j, 0)
-        # Fill in bfactors
-        counter = 0
-        for c in range(1, pose.pdb_info().num_chains() + 1):
-            chain_start = pose.conformation().chain_begin(c)
-            chain_end = pose.conformation().chain_end(c)
-            for i in range(chain_start + 3, chain_end - 3):
-                if counter < len(y_pred):
-                    for j in range(1, pose.residue(i).natoms() + 1):
-                        pose.pdb_info().bfactor(i, j, y_pred[counter])
-                    counter += 1
-        # Save pdb
-        pose.dump_pdb(args.output)
-
-    elif '3D' in mod:
-        # load model
-        custom_objects = {'ECCConv': ECCConv, 'GlobalSumPool': GlobalSumPool}
-        model = load_model(mod, custom_objects)
+    # load model
+    custom_objects = {'GATConv': GATConv, 'ECCConv': ECCConv, 'GlobalMaxPool': GlobalMaxPool}
+    model = load_model(mod, custom_objects)
 
         # Pick some decorators to add to your network
-        decorators = [decs.Rosetta_Ref2015_TwoBodyEneriges(individual=True, score_types=[ScoreType.fa_rep, ScoreType.fa_atr, ScoreType.fa_sol, ScoreType.lk_ball_wtd, ScoreType.fa_elec, ScoreType.hbond_sr_bb, ScoreType.hbond_lr_bb, ScoreType.hbond_bb_sc, ScoreType.hbond_sc])]
-        data_maker = mg.DataMaker(decorators=decorators, edge_distance_cutoff_A=8.0, max_residues=10, nbr_distance_cutoff_A=10.0)
-        #data_maker.summary()
+    decorators = [decs.SequenceSeparation(ln = True), decs.SimpleBBGeometry(use_nm = False), decs.Rosetta_Ref2015_TwoBodyEneriges(individual=True, score_types=[ScoreType.fa_rep, ScoreType.fa_atr, ScoreType.fa_sol, ScoreType.lk_ball_wtd, ScoreType.fa_elec, ScoreType.hbond_sr_bb, ScoreType.hbond_lr_bb, ScoreType.hbond_bb_sc, ScoreType.hbond_sc])]
+    data_maker = mg.DataMaker(decorators=decorators, edge_distance_cutoff_A=8.0, max_residues=10, nbr_distance_cutoff_A=10.0)
+    #data_maker.summary()
 
-        wrapped_pose = mg.RosettaPoseWrapper(pose)
-        cache = data_maker.make_data_cache(wrapped_pose)
+    wrapped_pose = mg.RosettaPoseWrapper(pose)
+    cache = data_maker.make_data_cache(wrapped_pose)
 
-        Xs = []
-        As = []
-        Es = []
+    Xs = []
+    As = []
+    Es = []
 
-        for resid in range(1, len(pose.sequence()) + 1):
-            X, A, E, _ = data_maker.generate_input_for_resid(wrapped_pose, resid, data_cache=cache)
-            Xs.append(X)
-            As.append(A)
-            Es.append(E)
+    for resid in range(1, len(pose.sequence()) + 1):
+        X, A, E, _ = data_maker.generate_input_for_resid(wrapped_pose, resid, data_cache=cache)
+        Xs.append(X)
+        As.append(A)
+        Es.append(E)
 
-        Xs = np.asarray(Xs)
-        As = np.asarray(As)
-        Es = np.asarray(Es) 
-        y_pred = model.predict([Xs, As, Es])
+    Xs = np.asarray(Xs)
+    As = np.asarray(As)
+    Es = np.asarray(Es) 
+    y_pred = model.predict([Xs, As, Es])
 
-        #create csv file
-        if args.csv:
-            rows = []
-            for c in range(1, pose.pdb_info().num_chains() + 1):
-                chain_start = pose.conformation().chain_begin(c)
-                chain_end = pose.conformation().chain_end(c)
-                for i in range(chain_start, chain_end + 1):
-                    if i-1 < len(y_pred):
-                        row = {
-                            'chain': pose.pdb_info().chain(i),
-                            'amino_acid': pose.residue(i).name(),
-                            'position_number': pose.pdb_info().number(i),
-                            'probability': y_pred[i-1]
-                        }
-                        rows.append(row)
-            df = pd.DataFrame(rows)
-            df.to_csv(args.csv, index=False)
-            print(f'Successfully generated {args.csv}')
-
-        # Map y_pred onto crystal structure and save pdb
-        # Set all bfactors to zero
-        for i in range(1, len(pose.sequence()) + 1):
-            for j in range(1, pose.residue(i).natoms() + 1):
-                pose.pdb_info().bfactor(i, j, 0)
-        # Fill in bfactors
-        counter = 0
+    #create csv file
+    if args.csv:
+        rows = []
         for c in range(1, pose.pdb_info().num_chains() + 1):
             chain_start = pose.conformation().chain_begin(c)
             chain_end = pose.conformation().chain_end(c)
             for i in range(chain_start, chain_end + 1):
-                if counter < len(y_pred):
-                    for j in range(1, pose.residue(i).natoms() + 1):
-                        pose.pdb_info().bfactor(i, j, y_pred[counter])
-                    counter += 1
-        # Save pdb
-        pose.dump_pdb(args.output)
+                if i-1 < len(y_pred):
+                    row = {
+                        'chain': pose.pdb_info().chain(i),
+                        'amino_acid': pose.residue(i).name(),
+                        'position_number': pose.pdb_info().number(i),
+                        'probability': y_pred[i-1]
+                        }
+                    rows.append(row)
+        df = pd.DataFrame(rows)
+        df.to_csv(args.csv, index=False)
+        print(f'Successfully generated {args.csv}')
 
-    else:
-        raise ValueError('Model name not known')
+        # Map y_pred onto crystal structure and save pdb
+        # Set all bfactors to zero
+    for i in range(1, len(pose.sequence()) + 1):
+        for j in range(1, pose.residue(i).natoms() + 1):
+            pose.pdb_info().bfactor(i, j, 0)
+    # Fill in bfactors
+    counter = 0
+    for c in range(1, pose.pdb_info().num_chains() + 1):
+        chain_start = pose.conformation().chain_begin(c)
+        chain_end = pose.conformation().chain_end(c)
+        for i in range(chain_start, chain_end + 1):
+            if counter < len(y_pred):
+                for j in range(1, pose.residue(i).natoms() + 1):
+                    pose.pdb_info().bfactor(i, j, y_pred[counter])
+                counter += 1
+        # Save pdb
+    pose.dump_pdb(args.output)
 
     # plot the predicted positions in ramachandran space
     if args.ramachandran:
