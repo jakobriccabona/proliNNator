4a95fd0dd5246ad2bd2a57968df2463e3945bd20
jakobriccabona <riccabona.jakob@gmail.com>
2025-10-20 13:28:43 +0200

    update testing script

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo docker run --rm -v $(pwd):/proliNNator/data prolinnator:v1 python proliNNator.py \
--i test/3ft7.pdb -p test/out.pdb -m 3D-model-v2.5.keras --csv test/out.csv\
---ramachandran test/out.png
+-i data/3ft7.pdb -p data/out.pdb -m 3D-model-v2.5.keras --csv data/out.csv\
+--ramachandran data/out.png
 
